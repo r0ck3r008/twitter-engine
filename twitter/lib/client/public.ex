d1265d2346353e5d21e62ab1e39f71a645d5168e
r0ck3r008 <naman.arora@tutanota.com>
2019-11-07 03:00:49 -0500

    signup is now a cast req from client side, rest same. Also, added login support

--- twitter/lib/client/public.ex
+++ twitter/lib/client/public.ex
@@ -1,7 +1,11 @@
 defmodule Twitter.Client.Public do
 
   def signup(client_pid, e_pid) do
-    GenServer.call(client_pid, {:signup, e_pid})
+    GenServer.cast(client_pid, {:signup, e_pid})
+  end
+
+  def login(client_pid, u_hash, e_pid) do
+    GenServer.cast(client_pid, {:login, u_hash, e_pid})
   end
 
 end
