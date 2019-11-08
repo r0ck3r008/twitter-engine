6f2411f47d592d7add709088157dc77d097709f4
r0ck3r008 <naman.arora@tutanota.com>
2019-11-07 23:53:21 -0500

    client received u_pid on login

--- twitter/lib/client/callbacks.ex
+++ twitter/lib/client/callbacks.ex
@@ -17,15 +17,21 @@ defmodule Twitter.Client do
   def handle_cast({:signup, e_pid},  _) do
     {:ok, u_pid}=Twitter.User.start_link(e_pid)
     {u_hash, cli_hash}=Twitter.User.Public.signup(u_pid, self())
-    {:noreply, u_hash}
+    {:noreply, {u_hash, u_pid, e_pid}}
   end
 
   @impl true
   def handle_cast({:login u_hash, e_pid},  _) do
-    Twitter.Engine.Public.login(e_pid, u_hash, self())
-    {:noreply, u_hash}
+    u_pid=Twitter.Engine.Public.login(e_pid, u_hash, self())
+    {:noreply, {u_hash, u_pid, e_pid}}
   end
   ###########signup related
+  
+  ###########follow related
+  @impl true
+  def handle_cast({:follow, to_hash}, e_pid) do
+
+  end
 
   @impl true
   def terminate(_, _) do
