3e127bf7f0f2c2362e1bef4735edad7203dccac2
r0ck3r008 <naman.arora@tutanota.com>
2019-11-09 00:37:50 -0500

    fixed the init from API to simulator

--- twitter/lib/init/init.ex
+++ twitter/lib/init/init.ex
@@ -4,23 +4,26 @@ defmodule Twitter.Init do
     #start the engine
     {:ok, e_pid}=Twitter.Engine.start_link
 
+    #start simulator
+    {:ok, sim_pid}=Twitter.Simulator.start_link(e_pid)
+
     #start clients
     clients=for _x<-0..num-1, do: Twitter.Client.start_link
 
     #start client signup process
-    tasks=for {_, client}<-clients, do: Task.async(fn-> task_fn(client, e_pid, 0) end)
+    tasks=for {_, client}<-clients, do: Task.async(fn-> task_fn(client, sim_pid, 0) end)
 
     #wait for tasks to finish
     for task<-tasks, do: Task.await(task, :infinity)
   end
 
-  def task_fn(client, e_pid, 0) do
-    Twitter.Api.signup(client, e_pid)
-    task_fn(client, e_pid, 1)
+  def task_fn(client, sim_pid, 0) do
+    Twitter.Simulator.Public.signup(sim_pid, client)
+    task_fn(client, sim_pid, 1)
   end
-  def task_fn(client, e_pid, count) do
+  def task_fn(client, sim_pid, count) do
     :timer.sleep(1000)
-    task_fn(client, e_pid, count)
+    task_fn(client, sim_pid, count)
   end
 
 end
