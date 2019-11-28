7279425c5405fb7d2f498046453b77d2294ea397
r0ck3r008 <naman.arora@tutanota.com>
2019-11-28 04:22:40 -0500

    completed the stat module

--- twitter/lib/stat/callbacks.ex
+++ twitter/lib/stat/callbacks.ex
@@ -10,17 +10,30 @@ defmodule Twitter.Stat do
   @impl true
   def init(:ok) do
     Logger.info("Starting Statictics module!")
-    {:ok, 0}
+    {:ok, {0, 0}}
   end
 
   @impl true
-  def handle_cast(:tweet_up, count) do
-    {:noreply, count+1}
+  def handle_cast(:count_up, {t0, count}) do
+    {:noreply, {t0, count+1}}
   end
 
   @impl true
-  def handle_call(:get_tweets, _from, count) do
-    {:reply, count, count}
+  def handle_cast(:start_timer, {_, count}) do
+    {:noreply, {System.monotonic_time, count}}
+  end
+
+  @impl true
+  def handle_call(:get_count, _from, {t0, count}) do
+    {:reply, count, {t0, count}}
+  end
+
+  @impl true
+  def handle_call(:stop_timer, _from, {t0, count}) do
+    {:reply,
+      System.convert_time_unit(System.monotonic_time-t0, :native, :milliseconds),
+      {t0, count}
+    }
   end
 
   @impl true
--- twitter/lib/stat/public.ex
+++ twitter/lib/stat/public.ex
@@ -1,11 +1,19 @@
 defmodule Twitter.Stat.Public do
 
-  def tweet_up(of) do
-    GenServer.cast(of, :tweet_up)
+  def count_up(of) do
+    GenServer.cast(of, :count_up)
   end
 
-  def get_tweet_count(of) do
-    GenServer.call(of, :get_tweets)
+  def get_count(of) do
+    GenServer.call(of, :get_count)
+  end
+
+  def start_timer(of) do
+    GenServer.cast(of, :start_timer)
+  end
+
+  def stop_timer(of) do
+    GenServer.call(of, :stop_timer)
   end
 
 end
